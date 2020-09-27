cf4c4971895260961613f82d9fd093f63859c5b3
Sophie Engle <sjengle@cs.usfca.edu>
2020-09-27 12:27:22 -0700

    Adding count tests for project 2.

---
+++ SearchEngineTest/src/test/java/CountOutputTest.java
@@ -0,0 +1,89 @@
+import java.nio.file.Path;
+import java.time.Duration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ * JUnit tests for the inverted index count functionality.
+ *
+ * @author CS 212 Software Development
+ * @author University of San Francisco
+ * @version Fall 2020
+ */
+@TestMethodOrder(OrderAnnotation.class)
+public class CountOutputTest extends TestUtilities {
+
+	/** Amount of time to wait for an individual test to finish. */
+	public static final Duration TIMEOUT = Duration.ofMinutes(1);
+
+	/**
+	 * Tests that parsing a single non-text file still works.
+	 */
+	@Order(1)
+	@Test
+	public void testMarkdownCounts() {
+		String filename = "counts-sentences.json";
+
+		Path input = Paths.TEXT.path.resolve("simple").resolve("sentences.md");
+		Path actual = Paths.ACTUAL.path.resolve(filename);
+		Path expected = Paths.EXPECTED.path.resolve("counts").resolve(filename);
+
+		String[] args = {
+				Flags.PATH.text, input.normalize().toString(),
+				Flags.COUNTS.text, actual.normalize().toString()
+		};
+
+		Assertions.assertTimeoutPreemptively(TIMEOUT, () -> {
+			checkOutput(args, actual, expected);
+		});
+	}
+
+	/**
+	 * Tests that parsing a single empty file still works.
+	 */
+	@Order(2)
+	@Test
+	public void testEmptyCounts() {
+		String filename = "counts-empty.json";
+
+		Path input = Paths.TEXT.path.resolve("simple").resolve("empty.txt");
+		Path actual = Paths.ACTUAL.path.resolve(filename);
+		Path expected = Paths.EXPECTED.path.resolve("counts").resolve(filename);
+
+		String[] args = {
+				Flags.PATH.text, input.normalize().toString(),
+				Flags.COUNTS.text, actual.normalize().toString()
+		};
+
+		Assertions.assertTimeoutPreemptively(TIMEOUT, () -> {
+			checkOutput(args, actual, expected);
+		});
+	}
+
+	/**
+	 * Tests the word counts functionality of the inverted index on the entire
+	 * input directory.
+	 */
+	@Order(3)
+	@Test
+	public void testCounts() {
+		String filename = "counts-text.json";
+
+		Path actual = Paths.ACTUAL.path.resolve(filename);
+		Path expected = Paths.EXPECTED.path.resolve("counts").resolve(filename);
+
+		String[] args = {
+				Flags.PATH.text, Paths.TEXT.path.normalize().toString(),
+				Flags.COUNTS.text, actual.normalize().toString()
+		};
+
+		Assertions.assertTimeoutPreemptively(TIMEOUT, () -> {
+			checkOutput(args, actual, expected);
+		});
+	}
+
+}
